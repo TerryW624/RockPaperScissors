1591e3ee1c9c7be19185366ca7e2319e9f5566af
Zachary Mcleod <zacharymcleod@zacharys-air.home>
2023-06-01 11:45:04 -0700

    feat: RockPaperScissors is updated to MVP. Not organized.

--- schoolYard.py
+++ schoolYard.py
@@ -8,29 +8,27 @@ class Rpsls:
         self.gestures = []
         self.player_1 = None
         self.player_2 = None
-
-    def score_increment(self, player):
-        score = player.score
-        score += 1
-        return score
     
     def two_out_three(self):
+        player_2_score = 0
+        player_1_score = 0
         while True:
             gesture1 = self.player_1.choose_gesture()
             gesture2 = self.player_2.choose_gesture()
+
             if self.player_2.name == "AI bot":
                 print(f"{self.player_2.name} chose {gesture2}") 
             if (gesture1 == "rock" and (gesture2 == "scissors" or gesture2 == "lizard")) or (gesture1 == "scissors" and (gesture2 == "paper" or gesture2 == "lizard")) or (gesture1 == "paper" and (gesture2 == "rock" or gesture2 == "spock")) or (gesture1 == "spock" and (gesture2 == "rock" or gesture2 == "scissors")) or (gesture1 == "lizard" and (gesture2 == "spock" or gesture2 == "paper")):
-                self.score_increment(self.player_1)
+                player_1_score += 1
             elif gesture1 == gesture2:
                 print("Tie")
                 continue
             else:
-                self.score_increment(self.player_2)
-            print(f"{self.player_1.name} {self.player_1.score} {self.player_2.name} {self.player_2.score}")
-            if self.player_1.score >= 2 or self.player_2.score >= 2:
+                player_2_score += 1
+            print(f"{self.player_1.name} {player_1_score} {self.player_2.name} {player_2_score}")
+            if player_1_score >= 2 or player_2_score >= 2:
                 break
-        if self.player_1.score > self.player_2.score:
+        if player_1_score > player_2_score:
             print(f"{self.player_1.name} wins")
         else:
             print(f"{self.player_2.name} wins")
